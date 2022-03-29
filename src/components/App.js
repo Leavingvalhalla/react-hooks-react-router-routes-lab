90345d8d416c93a5ab8bbb72f473392a54675509
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-29 13:50:36 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import Actors from "./Actors";
-import Directors from "./Directors";
-import Movies from "./Movies";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import Home from './Home';
+import Actors from './Actors';
+import Directors from './Directors';
+import Movies from './Movies';
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <NavBar />
+      <Route path="/movies">
+        <Movies />
+      </Route>
+      <Route path="/directors">
+        <Directors />
+      </Route>
+      <Route path="/actors">
+        <Actors />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
